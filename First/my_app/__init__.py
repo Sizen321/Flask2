147fb83aaf36aa846a33be1846e74de819077eae
Sizen321 <Sizen321@mail.ru>
2024-12-04 22:30:35 +0300

    Add config

--- First/my_app/__init__.py
+++ First/my_app/__init__.py
@@ -4,3 +4,9 @@ from my_app.hello_as_bp.handlers import hello
 
 app = Flask(__name__)
 app.register_blueprint(hello)
+
+# print(app.default_config)
+app.config.from_object("config.DevelopmentConfig")
+
+# print("\n=========== after loading ============\n")
+# print(app.config)
